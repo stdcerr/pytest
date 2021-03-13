901fee4114bc94f42eca0715e735be48ccce1cd2
stdcerr <stdcerr@yandex.com>
2021-03-13 11:56:17 -0800

    Added multithread.py with QThreadPool & QRunnable

---
+++ multithread.py
@@ -0,0 +1,71 @@
+#some source information: https://www.learnpyqt.com/tutorials/multithreading-pyqt-applications-qthreadpool/
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import time
+
+class Worker(QRunnable):
+    '''
+    Worker thread
+    '''
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    @pyqtSlot()
+    def run(self):
+        self.fn (*self.args, **self.kwargs)
+
+class MainWindow(QMainWindow):
+
+
+    def __init__(self, *args, **kwargs):
+        self.threadpool = QThreadPool()
+        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.counter = 0
+
+        layout = QVBoxLayout()
+
+        self.l = QLabel("Start")
+        b = QPushButton("Test")
+        b.pressed.connect(self.oh_no)
+
+        layout.addWidget(self.l)
+        layout.addWidget(b)
+
+        w = QWidget()
+        w.setLayout(layout)
+
+        self.setCentralWidget(w)
+
+        self.show()
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def execute_this_fn(self):
+        for x in range (500000):
+            print("Hello {}".format(x))
+            #time.sleep(2)
+
+    def oh_no(self):
+        worker = Worker(self.execute_this_fn)
+
+        self.threadpool.start(worker)
+
+    def recurring_timer(self):
+        self.counter +=1
+        self.l.setText("Counter: %d" % self.counter)
+
+
+app = QApplication([])
+window = MainWindow()
+app.exec_()
